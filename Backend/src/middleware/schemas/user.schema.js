74fecd96b6309da9029d126e0c4a0bfca9f1f0f6
LeonardoTelez <telezleonardo@gmail.com>
2025-11-18 18:16:17 -0300

    correccion de schemas

--- Backend/src/middleware/schemas/user.schema.js
+++ Backend/src/middleware/schemas/user.schema.js
@@ -2,15 +2,20 @@ import Joi from "joi";
 
 const userSchema = {
     create: Joi.object({
-        dni: Joi.string().pattern(/^\d+$/).length(8).required().messages({
-                'string.pattern.base': 'El DNI debe contener solo números.',
-                'string.length': 'El DNI debe tener exactamente 8 dígitos.'
-            }),
+
         name: Joi.string().required(),
+
         surname: Joi.string().required(),
+
         email: Joi.string().email().required().messages({
             "string.email": "El email no es válido."
         }),
+
+        dni: Joi.string().pattern(/^\d+$/).length(8).required().messages({
+            'string.pattern.base': 'El DNI debe contener solo números.',
+            'string.length': 'El DNI debe tener exactamente 8 dígitos.'
+        }),
+
         password: Joi.string().min(8).required().messages({
             "string.min": "La contraseña debe tener al menos 8 caracteres."
         }),
@@ -20,21 +25,27 @@ const userSchema = {
         email: Joi.string().email().required().messages({
             "string.email": "El email no es válido."
         }),
+
         password: Joi.string().min(8).required().messages({
             "string.min": "La contraseña debe tener al menos 8 caracteres."
         }),
     }),
 
     update: Joi.object({
+    
+        name: Joi.string(),
+
+        surname: Joi.string(),
+
+        email: Joi.string().email().messages({
+            "string.email": "El email no es válido."
+        }),
+
         dni: Joi.string().pattern(/^\d+$/).length(8).messages({
             'string.pattern.base': 'El DNI debe contener solo números.',
             'string.length': 'El DNI debe tener exactamente 8 dígitos.'
         }),
-        name: Joi.string(),
-        surname: Joi.string(),
-        email: Joi.string().email().messages({
-            "string.email": "El email no es válido."
-        }),
+        
         password: Joi.string().min(8).messages({
             "string.min": "La contraseña debe tener al menos 8 caracteres."
         }),
