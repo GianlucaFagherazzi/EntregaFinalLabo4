0f24a27ea211a773667f7d3b9f5640481c6d8928
LeonardoTelez <telezleonardo@gmail.com>
2025-11-18 17:55:02 -0300

    correccion de migration

--- Backend/src/migrations/5.create.products.js
+++ Backend/src/migrations/5.create.products.js
@@ -4,14 +4,14 @@ export default {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('products', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-      description: { type: Sequelize.STRING, allowNull: true },
       name: { type: Sequelize.STRING, allowNull: false },
+      description: { type: Sequelize.STRING, allowNull: true },
       price: { type: Sequelize.DOUBLE, allowNull: false },
       stock: { type: Sequelize.DOUBLE, allowNull: false },
       isActive: { type: Sequelize.BOOLEAN, defaultValue: true },
 
-      userId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onDelete: 'CASCADE', onUpdate: 'CASCADE' },
-      categoryId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'category', key: 'id' }, onDelete: 'CASCADE', onUpdate: 'CASCADE' }
+      userId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }},
+      categoryId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'category', key: 'id' }}
     });
   },
 
--- Backend/src/migrations/6.create.movements.js
+++ Backend/src/migrations/6.create.movements.js
@@ -1,20 +1,13 @@
 'use strict';
 
-import { sequelize } from "../config/database";
-
 export default {
     async up(queryInterface, Sequelize) {
         await queryInterface.createTable('movements', {
             id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-            date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
-            type: { type: Sequelize.STRING, allowNull: false },
-            quantity: { type: Sequelize.INTEGER, allowNull: false },
-            amount: { type: Sequelize.DOUBLE, allowNull: false },
-            
-            userBuyerId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onDelete: 'CASCADE' },
-            userSellerId: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onDelete: 'CASCADE' },
-            productId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'products', key: 'id' }, onDelete: 'CASCADE' },
-            tarjetId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'tarjets', key: 'id' }, onDelete: 'CASCADE' }
+            productId: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'products', key: 'id' }},
+            quantity: { type: Sequelize.DOUBLE, allowNull: false },
+            totalAmount: { type: Sequelize.DOUBLE, allowNull: false },
+            date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
         });
     },
 
