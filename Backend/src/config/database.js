0cdbff9d77debd210a4f71b5cd48451f8cff6ea1
GianlucaFagherazzi <fagherazzigianluca@gmail.com>
2026-04-07 17:53:00 -0300

    preparacion para deploy

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,18 +1,28 @@
 // Configuracion general de la base de datos con sequelize
-import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv' 
+import { Sequelize } from "sequelize"
+import dotenv from "dotenv"
 
 dotenv.config()
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false
-  }
-)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false }
+      }
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        logging: false
+      }
+    )
 
+export default sequelize
